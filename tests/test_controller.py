0f9cb820f28b010bfd5c63077d4288ebf41ce639
chriscardillo <cfcardillo23@gmail.com>
2021-08-30 18:50:41 -0400

    black formatting

--- tests/test_controller.py
+++ tests/test_controller.py
@@ -2,12 +2,15 @@ import pytest
 import uuid
 from declare import Controller
 
+
 @pytest.fixture
 def controller():
     return Controller("app")
 
+
 def test_controller_name(controller):
     assert controller.name == "app"
+
 
 def test_controller_has_uuid(controller):
     assert isinstance(controller.uuid, uuid.UUID)
--- tests/test_stateful.py
+++ tests/test_stateful.py
@@ -2,12 +2,15 @@ import pytest
 import uuid
 from declare import Stateful
 
+
 @pytest.fixture
 def stateful():
     return Stateful("app")
 
+
 def test_stateful_name(stateful):
     assert stateful.name == "app"
+
 
 def test_controller_has_uuid(stateful):
     assert isinstance(stateful.uuid, uuid.UUID)
