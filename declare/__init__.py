d0ba6998773fac701efb807fce0781e49eadf2be
chriscardillo <cfcardillo23@gmail.com>
2021-08-30 19:30:33 -0400

    BaseClass

--- declare/__init__.py
+++ declare/__init__.py
@@ -1,13 +1,17 @@
 import uuid
 
 
-class Controller:
+class BaseClass:
     def __init__(self, name, *args, **kwargs):
         self.uuid = uuid.uuid4()
         self.name = name
 
 
-class Stateful:
-    def __init__(self, name, *args, **kwargs):
-        self.uuid = uuid.uuid4()
-        self.name = name
+class Controller(BaseClass):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+
+class Stateful(BaseClass):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
